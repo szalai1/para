40b1621d83ee50769560799deddfa8b7aac88d37
Péter Szalai <szalaipeti.vagyok@gmail.com>
2015-09-24 21:49:29 +0200

    conv

---
+++ filt/conv.cpp
@@ -0,0 +1,27 @@
+/*
+
+ * beolvassa a parameterkent kapott DIMX * DIMX mereru bitmap file-t, amelyen 
+ * elkiemelest vegez, es kiirja a pic.out file-ba az eredmenyt.
+ * DIMX, DIMY, valamint az eredmeny file neve be van drotozva a programba!
+ *
+ * TODO: pgm formatum ertelmezses/eloallitasa
+ * 	pgm header: "P5\nW\nH\n255\n"
+ *      ahol W - szelesseg
+ * 	     H - magassag
+ * 
+ */
+
+#include <iostream>
+#include "image.h"
+
+char M[3][3]= {				// konvolucios matrix
+	{ 0, 1, 0, },
+	{ 1,-4, 1, },
+	{ 0, 1 ,0  } };
+
+
+
+int main(int argc, char *argv[]) {
+	Image img(argv[1]);
+}
+
