8ce97886d2fd4f68c0ff932c938d7bae974fbaaf
Péter Szalai <szalaipeti.vagyok@gmail.com>
2015-09-27 20:22:22 +0200

    buggy

--- filt/conv.cpp
+++ filt/conv.cpp
@@ -11,8 +11,8 @@ int main(int argc, char *argv[]) {
   int size , rank;
   //MPI_Comm_size(MPI_COMM_WORLD, &size);
   //MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  std::cout << size << " " << rank << std::endl;
 	Image img(argv[1]);
+  std::cout << img.get_sizex() << " " << img.get_sizey() << std::endl;
   //  Image partI  = img.get_stripex(100,400);
   img.convolute_lineX(150, M);
   img.save("part2.pgm");
